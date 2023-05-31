59f35f83eddbeeac90be932dffcfbde1b2ec2b1f
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-30 23:31:39 -0300

    seccion que idica el turno

--- Tateti-en-React/src/App.jsx
+++ Tateti-en-React/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 const TURNS = {
@@ -5,9 +6,9 @@ const TURNS = {
   O: 'o'
 }
 
-const board = Array(9).fill(null)
 
-const Square = ({ children, updateBoard, index })=> {
+const Square = ({ children, isSelected, updateBoard, index })=> {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
   return(
     <div className='square'>
       {children}
@@ -16,6 +17,9 @@ const Square = ({ children, updateBoard, index })=> {
 }
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS,X)
+  
   return (
     <main className='board'>
       <h1>Ta Te Ti</h1>
@@ -25,12 +29,22 @@ function App() {
             return (
               <Square
               key={index}
-              index={index}>
-                {index}
+              index={index}
+              >
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={ turn === TURNS.X }>
+          { TURNS.X }
+        </Square>
+        <Square isSelected={ turn === TURNS.O }>
+          { TURNS.O }
+        </Square>
       </section>
     </main>
   )
